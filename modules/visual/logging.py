5995c97a6e71490a97276e2ac1c71b62fec53623
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-07-29 00:13:43 +0200

    Update logging.py

--- modules/visual/logging.py
+++ modules/visual/logging.py
@@ -3,5 +3,8 @@ def speculos_lotus():
 * NOTE *: 
     + You can add by yourself some others mail domains (ex @mail.mail) 
     to search for target emails using that domain.
+        HOW :
+          . Go to /modules
+          . Add your domains 
 
     """)
