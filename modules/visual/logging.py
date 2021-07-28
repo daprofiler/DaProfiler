2d8857e5996d88a5eb8a18ec8cb31f642d251627
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-07-29 00:33:26 +0200

    [MOD] Edited banner, Fixed banner error while upt

--- modules/visual/logging.py
+++ modules/visual/logging.py
@@ -1,16 +1,16 @@
-from colorama import Fore, init
-init(autoreset=True)
-
 def speculos_lotus():
-    with open("modules/mail_domain.txt","r") as file:
-        lines = file.readlines()
-        file.close()
-    print("""
+    try:
+        with open("modules/mail_domain.txt","r") as file:
+            lines = file.readlines()
+            file.close()
+        print("""
 * NOTE *: 
     + You can add by yourself some others mail domains (ex @mail.mail) 
     to search for target emails using that domain.
         HOW :
-          . Go to /modules
-          . Add your domains ({} Detected) 
+        . Go to /modules
+        . Add your domains ({} Detected) 
 
-    """.format(Fore.GREEN+str(len(lines))+Fore.RESET))
+        """.format(str(len(lines))))
+    except:
+        pass
