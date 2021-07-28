c6d4dc0278492723d442739d668984200954685b
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-07-28 18:56:25 +0200

    Update logging.py

--- modules/visual/logging.py
+++ modules/visual/logging.py
@@ -1,15 +1,7 @@
-from datetime import datetime
-import time
+def speculos_lotus():
+    print("""
+* NOTE *: 
+    + You can add by yourself some others mail domains (ex @mail.mail) 
+    to search for target emails using that domain.
 
-def terminal_loggin(log,text):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    text = (current_time, text)
-    try:
-        if log.lower() == "true":
-            print ("\033[A                             \033[A")
-            for i in text: 
-                print(i, end ='\r')
-                time.sleep(0.01)
-    except:
-        pass
+    """)
