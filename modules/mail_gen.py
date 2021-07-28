2710de6d3c3d9d418736b04781036b0fd0e25695
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-07-28 14:29:07 +0200

    Update mail_gen.py

--- modules/mail_gen.py
+++ modules/mail_gen.py
@@ -9,13 +9,11 @@ def check(name,pren):
         "{}.{}@gmail.com".format(name,pren),
         "{}.{}@yahoo.com".format(name,pren),
         "{}{}@yahoo.com".format(name,pren),
-        "{}{}@yahoo.fr".format(name,pren),
-        "{}.{}@aol.com".format(name,pren),
-        "{}{}@aol.com".format(name,pren),
+        #"{}{}@yahoo.fr".format(name,pren),
         "{}.{}@hotmail.com".format(name,pren),
         "{}{}@hotmail.com".format(name,pren),
-        "{}{}@hotmail.fr".format(name,pren),
-        "{}{}@outlook.fr".format(name,pren),
+        #"{}{}@hotmail.fr".format(name,pren),
+        #"{}{}@outlook.fr".format(name,pren),
         "{}.{}@outlook.com".format(name,pren),
         "{}{}@outlook.com".format(name,pren),    
     ]
@@ -52,8 +50,8 @@ def skype2email(name,pren):
             i+"@yahoo.com",
             i+"@gmail.com",
             i+"@hotmail.com",
-            i+"@hotmail.fr",
-            i+"@outlook.fr",
+            i+"@icloud.com",
+            i+"@yandex.ru",
             i+"@outlook.com"
         ]
         for i in emails:
