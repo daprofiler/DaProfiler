1e2d090f037f83b2bddf2f830d6973f3f1c9c0da
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-08-10 14:52:04 +0200

    [ADD] Twitter search using dorking

--- modules/twitter_search.py
+++ modules/twitter_search.py
@@ -1,29 +1,9 @@
-import requests, bs4
-from bs4 import BeautifulSoup
+from googlesearch import search
 
 def twitter_search(name,pren):
-    try:
-        url = "https://www.twuko.com/search?term={}+{}&type=user".format(pren,name)
-
-        r = requests.get(url)
-        page = r.content
-        features = "html.parser"
-        soup = BeautifulSoup(page, features)
-
-        full_name = soup.find_all('div',{'class':'flex items-center'})
-        username  = soup.find_all('a',{'class':'text-sm'})
-
-        final_accounts = []
-
-        for i in range(len(full_name)):
-            usernamee = username[i].text
-            fullname  = full_name[i].text
-            final_accounts.append(
-                '{}\t|{}'.format(usernamee,fullname)
-            )
-        if len(final_accounts) == 0:
-            return None
-        else:
-            return final_accounts
-    except:
+    dork = 'site:twitter.com intitle:") | Twitter" "{} {}"'.format(pren,name)
+    results = search(dork,num_results=1)
+    if len(results) > 0:
+        return(results[0])
+    else:
         return None
