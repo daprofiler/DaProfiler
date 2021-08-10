c0a6541b1aa60cc70c092049380ae9d995baaefd
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-08-10 14:53:17 +0200

    Update logging.py

--- modules/visual/logging.py
+++ modules/visual/logging.py
@@ -5,6 +5,8 @@ def speculos_lotus():
             file.close()
         print("""
 * NOTE *: 
+    + You can update DaProfiler using : py profiler.py -u True
+    
     + You can add by yourself some others mail domains (ex @mail.mail) 
     to search for target emails using that domain.
         HOW :
