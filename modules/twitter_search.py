bbfa4efdfd97ff9a3b4f07f0c5935a7b41fe412f
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-08-10 14:54:36 +0200

    [ADD] Twitter search using dorking

--- modules/twitter_search.py
+++ modules/twitter_search.py
@@ -4,6 +4,6 @@ def twitter_search(name,pren):
     dork = 'site:twitter.com intitle:") | Twitter" "{} {}"'.format(pren,name)
     results = search(dork,num_results=1)
     if len(results) > 0:
-        return(results[0])
+        return(results)
     else:
         return None
