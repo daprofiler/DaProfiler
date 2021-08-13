0b52ee133ca56cc565f23b57b36b56ca745dd327
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-08-13 21:20:46 +0200

    [MOD] DeathRecords module shows death ages

--- modules/death_records.py
+++ modules/death_records.py
@@ -11,17 +11,23 @@ def death_search(name,pren):
 
         names   = soup.find_all('h2')
         villes  = soup.find_all('span',{'class':'ville'})
+        ages    = soup.find_all('p',{'class':'list-item__adress'})
+        links   = soup.find_all('a',{'class':'list__link'})
 
         profile_list = []
 
         for i in range(len(names)):
             name = names[i].text.strip()
             loc  = villes[i].text.strip()
-            profile_list.append({'Name':name,'Loc':loc.replace('- ','')})
+            age  = ages[i].text.strip()
+            link = links[i]
+            link = (str(link).split('" title="')[0])
+            link = str(link).replace('<a class="list__link" href="','').replace('https://www.','')
+            profile_list.append({'Name':name,'Link':link,'Age':str(age)[0:2].replace('ans','').strip(),'Loc':loc.replace('- ','')})
 
         if len(profile_list) == 0:
             return None
         else:
             return profile_list
-    except:
+    except KeyboardInterrupt:
         return None
