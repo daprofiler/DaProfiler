12d281696ba8feb9b012a3ef6e2c3ae9e18fd2c4
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-08-25 18:44:44 +0200

    [MOD] LinkedIN Search more accurate

--- modules/linkedin_search.py
+++ modules/linkedin_search.py
@@ -3,6 +3,7 @@ from googlesearch import search
 def linkedin_search(name,pren):
     a = search('"{} {}" -intitle:"profiles" -inurl:"dir/ " site:fr.linkedin.com/in/ OR site:fr.linkedin.com/pub/'.format(pren,name), lang="fr")
     if len(a) > 0:
-        return a[0]
+        if name.lower() in a[0] and pren.lower() in a[0]:
+            return a[0]
     else:
         return None
