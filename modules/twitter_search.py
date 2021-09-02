2d76de1e9c47f9a86953d87bdfbb063f3e1ab017
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-09-03 00:25:59 +0200

    [FIX] Requests.exceptions.httperror FIXED

--- modules/twitter_search.py
+++ modules/twitter_search.py
@@ -1,9 +1,13 @@
-from googlesearch import search
+from googlesearch        import search
+from requests.exceptions import HTTPError
 
 def twitter_search(name,pren):
-    dork = 'site:twitter.com intitle:") | Twitter" "{} {}"'.format(pren,name)
-    results = search(dork,num_results=1)
-    if len(results) > 0:
-        return(results)
-    else:
+    try:
+        dork = 'site:twitter.com intitle:") | Twitter" +{} +{}'.format(pren,name)
+        results = search(dork,num_results=1)
+        if len(results) > 0:
+            return(results)
+        else:
+            return None
+    except HTTPError:
         return None
