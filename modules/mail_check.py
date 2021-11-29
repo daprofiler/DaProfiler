d9b41a742c53eb4d6244d3531e870d825af111ba
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2021-11-29 10:50:53 +0100

    Email verification working

--- modules/mail_check.py
+++ modules/mail_check.py
@@ -1,10 +1,9 @@
-#trouvé sur "https://docs.isitarealemail.com/how-to-validate-email-addresses-in-python"
-# modifié (un peu) par eupone
 import requests
 
 def verify(mail):
-    response = requests.get("https://isitarealemail.com/api/email/validate?email={}".format(mail),params = {'Authorization':'fa86a707-750e-485c-8ec3-86eddd7ec4d0'},headers = {'Authorization': "Bearer fa86a707-750e-485c-8ec3-86eddd7ec4d0"})
+    response = requests.get("https://isitarealemail.com/api/email/validate",params = {'email': mail},headers = {'Authorization': "Bearer " + "21000771-6911-402d-9d32-7697238803e8" })
     try:
+        print(response.text)
         data = response.json()
         status = data['status']
         if status == "valid":
