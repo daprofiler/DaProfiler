18e56f01527acc93f5fd767865c7162f5cd0490a
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2022-07-04 13:36:41 +0200

    [FIX] & [ADD]
    Fixed exception
    Added name to json output

--- modules/last_diplomes.py
+++ modules/last_diplomes.py
@@ -31,13 +31,13 @@ def last_diplomes_bac(name,pren):
             }
             return json_output
 
-def last_diplomes(name,pren):
+def last_diplomes_brevet(name,pren):
     url = f'https://resultats.etudiant.lefigaro.fr/resultats-brevet/recherche?name={pren} {name}&city_label=&city_insee='
     r = requests.get(url)
     page = r.content
     features = "html.parser"
     soup = BeautifulSoup(page, features)
-    profiles = soup.find_all('div',{'class':'candidate'})    
+    profiles = soup.find_all('div',{'class':'bg-white p-2'})    
     for i in profiles:
         profile = str(i).lower()
         if pren.lower() in profile and name.lower() in profile:
@@ -46,17 +46,26 @@ def last_diplomes(name,pren):
             page = r.content
             features = "html.parser"
             soup = BeautifulSoup(page, features)
-            academie = str(soup.find('span',{'class':'subtitle'})).split('">')[1].split('</')[0]
-            mention  = str(soup.find('p',{'class':'mention'})).split('">')[1].split('<')[0].strip()
-            ville    = str(soup.find('div',{'class':'infos'})).split('href')[1].split('</a>')[0].split('">')[1]
-            diplome  = str(soup.find('div',{'class':'infos'})).split('<p>')[1].split('</p>')[0]
             
+            profile = soup.find('div',{'class':'box'})
+            mention = soup.find('span',{'class':'block text-4xl text-red'})
+            diplome = soup.find('p',{'class':'text-grey-600 mb-1'})
+
+            diplome    = str(diplome).split('text-grey-600 mb-1">')[1].split('</p>')[0]
+            nom_prenom = str(profile).split('h1>')[1].split('<span')[0]
+            ville      = str(profile).split('capitalize">')[1].split('</span>')[0]
+            academie   = str(profile).split('(')[1].split(')')[0]
+            mention    = str(mention).split('class="block text-4xl text-red">')[1].split('</span>')[0]
+
+
             json_output = {
                 'Exists':True,
+                "Name":nom_prenom.strip(),
                 'academie':academie,
                 'Link':url,
                 'mention':mention,
                 'ville':ville,
                 'Diplome':diplome
             }
+
             return json_output
