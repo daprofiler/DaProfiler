8c0b1b523608ba960030d0fbdf8dbb61dd8842a7
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2022-07-20 19:09:53 +0200

    [FIX] Baccaleureat search ready to use

--- modules/last_diplomes.py
+++ modules/last_diplomes.py
@@ -7,30 +7,43 @@ def last_diplomes_bac(name,pren):
     page = r.content
     features = "html.parser"
     soup = BeautifulSoup(page, features)
-    profiles = soup.find_all('div',{'class':'candidate'})    
+    profiles = soup.find_all('td',{'class':'svelte-11did2l'})   
+    listt = [] 
     for i in profiles:
-        profile = str(i).lower()
-        if pren.lower() in profile and name.lower() in profile:
-            url = "https://resultats.etudiant.lefigaro.fr"+profile.split('href')[1].split('"')[1]
-            r = requests.get(url)
-            page = r.content
+        if len(listt) == 1:
+            subject = listt[0]
+            link = "https://resultats.etudiant.lefigaro.fr"+subject.split('href="')[1].split('">')[0]
+
+            academie = None
+            mention  = None
+            ville    = None
+            diplome  = None
+
+            r        = requests.get(link)
+            page     = r.content
             features = "html.parser"
-            soup = BeautifulSoup(page, features)
-            academie = str(soup.find('span',{'class':'subtitle'})).split('">')[1].split('</')[0]
-            mention  = str(soup.find('p',{'class':'mention'})).split('">')[1].split('<')[0].strip()
-            ville    = str(soup.find('div',{'class':'infos'})).split('href')[1].split('</a>')[0].split('">')[1]
-            diplome  = str(soup.find('div',{'class':'infos'})).split('<p>')[1].split('</p>')[0]
-            
+            soup     = BeautifulSoup(page, features)
+
+            mention  = soup.find('span',{'class':'block text-4xl text-red'}).text.split('"')[1].split('"')[0]
+            diplome  = soup.find('p',{'class':'text-grey-600 mb-1'}).text.strip()
+            academie = soup.find('div',{'class':'flex flex-col items-center sm:flex-row flex-wrap gap-5'}).text.split(',')[1].strip()
+            city     = soup.find('a',{'class':'capitalize underline'}).text.strip()
+
             json_output = {
                 'Exists':True,
                 'academie':academie,
-                'Link':url,
+                'Link':link,
                 'mention':mention,
-                'ville':ville,
+                'ville':city,
                 'Diplome':diplome
             }
             return json_output
+        elif len(listt) == 0:
+            profile = str(i).lower()
+            if pren.lower() in profile and name.lower() in profile:
+                    listt.append(profile)
 
+                    
 def last_diplomes_brevet(name,pren):
     url = f'https://resultats.etudiant.lefigaro.fr/resultats-brevet/recherche?name={pren} {name}&city_label=&city_insee='
     r = requests.get(url)
@@ -67,5 +80,4 @@ def last_diplomes_brevet(name,pren):
                 'ville':ville,
                 'Diplome':diplome
             }
-
             return json_output
