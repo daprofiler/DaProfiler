1a3b27b2262a9c931baa47fc38877df44d34f464
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2022-07-27 20:46:04 +0200

    [UPT] Added LinkedIN API

--- modules/linkedin_search.py
+++ modules/linkedin_search.py
@@ -1,4 +1,5 @@
 from googlesearch import search
+from linkedin_api import Linkedin
 
 def linkedin_search(name,pren):
     try:
@@ -10,3 +11,43 @@ def linkedin_search(name,pren):
             return None
     except:
         pass
+
+def official_linkedin_search(name,pren):
+    username = ""
+    password = ""
+
+    if len(username) == 0:
+        return None
+    else:
+        api = Linkedin(username=username,password=password)
+
+        profile_data = api.search_people(keyword_first_name=pren,keyword_last_name=name)
+
+        if len(profile_data) == 0:
+            return None
+        else:
+            public_id       = profile_data[0]['public_id']
+            urn_id          = profile_data[0]['urn_id']
+            profile_infos   = api.get_profile(urn_id=urn_id,public_id=public_id)
+            profile_contact = api.get_profile_contact_info(urn_id=urn_id,public_id=public_id)
+            email_adress    = profile_contact['email_address']
+            twitters        = profile_contact['twitter']
+            birthdate       = profile_contact['birthdate']
+            phone_numbers   = profile_contact['phone_numbers']
+            try:
+                job = (profile_infos['headline'])
+            except:
+                job = None
+
+            data = {
+                'Job':job,
+                'email':email_adress,
+                'urnid':urn_id,
+                'url':'https://www.linkedin.com/in/'+public_id,
+                'twitters':twitters,
+                'birthdate':birthdate,
+                'phone_numbers':phone_numbers
+            }
+            return data
+
+
