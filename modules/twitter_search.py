de8951edaaa9e30d961dcbca2760fa071efb5ca4
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2022-08-15 15:12:28 +0200

    [UPT] Twitter search

--- modules/twitter_search.py
+++ modules/twitter_search.py
@@ -1,13 +1,19 @@
-from googlesearch        import search
-from requests.exceptions import HTTPError
+import requests, bs4
+from bs4 import BeautifulSoup
 
 def twitter_search(name,pren):
-    try:
-        dork = 'site:twitter.com intitle:") | Twitter" +{} +{}'.format(pren,name)
-        results = search(dork,num_results=1)
-        if len(results) > 0:
-            return(results)
-        else:
-            return None
-    except HTTPError:
-        return None
+    url = "https://www.sotwe.com/search/{} {}".format(pren,name)
+    r = requests.get(url)
+    page = r.content
+    features = "html.parser"
+    soup = BeautifulSoup(page, features)
+
+    usernames = []
+
+    username = soup.find_all('div',{'class':'v-list-item__subtitle caption'})
+    
+    for i in username:
+        usernames.append(i.text.strip())
+
+    return usernames
+
