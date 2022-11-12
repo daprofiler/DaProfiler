134239b88f88b89aaafc530ceae0beb2933a5679
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2022-11-12 13:10:55 +0100

    Update mail_gen.py

--- modules/mail_gen.py
+++ modules/mail_gen.py
@@ -1,29 +1,31 @@
 import threading, requests, bs4
-from tqdm         import tqdm
 from bs4          import BeautifulSoup
 from modules      import mail_check
-from googlesearch import search
+#from googlesearch import search
 
 def check(name,pren):
+    print("📪 MailBox guessing ...")
     name = name.lower()
     pren = pren.lower()
     results = [
         "{}.{}@gmail.com".format(name,pren),
-        "{}.{}@yahoo.com".format(name,pren),
-        "{}{}@yahoo.com".format(name,pren),
-        "{}{}@yahoo.fr".format(name,pren),
         "{}.{}@hotmail.com".format(name,pren),
         "{}{}@hotmail.com".format(name,pren),
         "{}{}@hotmail.fr".format(name,pren),
         "{}{}@outlook.fr".format(name,pren),
         "{}.{}@outlook.com".format(name,pren),
-        "{}{}@outlook.com".format(name,pren),    
+        "{}{}@outlook.com".format(name,pren), 
+        "{}.{}@gmail.com".format(pren,name),
+        "{}.{}@hotmail.com".format(name,pren),
+        "{}{}@hotmail.com".format(pren,name),
+        "{}{}@hotmail.fr".format(pren,name),
+        "{}{}@outlook.fr".format(pren,name),
+        "{}.{}@outlook.com".format(pren,name),
+        "{}{}@outlook.com".format(pren,name),    
     ]
     valid_mails = []
-    bar = tqdm(desc='Mail permutation',total=len(results))
     for i in results:
         a = mail_check.verify(mail=i)
-        bar.update(1)
         if a is not None:
             valid_mails.append(i)
     return valid_mails
@@ -56,16 +58,15 @@ def skype2email(name,pren):
         for line in lines:
             if "@" in line and "." in line:
                 emails.append(i+line)
-        bar = tqdm(desc="Searching for target emails on each domains".format(line.split('@')[1]),total=len(emails))
         for i in emails:
             a = mail_check.verify(mail=i.strip())
-            bar.update(1)
             if a is not None:
                 valid_emails.append(i.strip())
     return valid_emails
 
 def pinterest2email(name,pren):
     try:
+        """
         therm = 'allintitle: {} {}"Profil de {} {}" site:pinterest.com -pin'.format(pren,name,pren,name)
 
         a = search(therm, lang="fr")
@@ -88,5 +89,7 @@ def pinterest2email(name,pren):
             return valid_emails
         else:
             return None
+        """
+        return None
     except:
         return None
