abc3fa266e82a86fc66adc20e393ef94f4a6668d
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2023-02-14 12:14:10 +0100

    [FIX] Death record source url

--- modules/official_documents/death_records.py
+++ modules/official_documents/death_records.py
@@ -1,26 +1,21 @@
-import profile
-import requests,bs4
-from bs4 import BeautifulSoup
-
 def death_search(name,pren):
     try:
-        url = "https://www.libramemoria.com/avis?nom={}&prenom={}&debut=&fin=&departement=&commune=&communeName=&titre=".format(name,pren)
+        url = "https://avis-deces.linternaute.com/recherche-avis?q={} {}".format(name,pren)
         r = requests.get(url)
         page = r.content
         features = "html.parser"
         soup = BeautifulSoup(page, features)
 
-        names  = soup.find_all('div',{'class':'cellule nom alone'})
-        ages   = soup.find_all('div',{'class':'cellule age hideXs_tablecell alone'})
-        villes = soup.find_all('div',{'class':'cellule ville liste_virgule alone'})
-
+        names  = soup.find_all('h4')
+        ages   = soup.find_all('small')
+        villes = soup.find_all('div',{'class':'odResultList__details--death'})
         profile_list = []
 
         for i in range(len(names)):
             try:
                 name = names[i].text.split('(')[0].replace('\r','').replace('\n','').replace('\t','').strip()
-                loc  = villes[i].text.replace('\r','').replace('\n','').replace('\t','').replace('(',' (').strip()
-                age  = ages[i].text.replace("ans","Years old").strip()
+                loc  = villes[i].text.split('à')[1].replace('\n','').replace('                     ','').strip()
+                age  = ages[i].text.strip()
                 dictt = {'Name':name,'Age':str(age),'Loc':loc.replace('- ','')}
                 profile_list.append(dictt)
             except:
@@ -31,5 +26,4 @@ def death_search(name,pren):
         else:
             return profile_list
 
-    except KeyboardInterrupt:
-        return None
+    except KeyboardInter
