317e360f5da38fbbd436f7e286618a89cb05d550
Dalunacrobate <86521641+TheRealDalunacrobate@users.noreply.github.com>
2023-02-19 11:40:12 +0100

    [FIX] TimeoutError

--- modules/mail/mail_check.py
+++ modules/mail/mail_check.py
@@ -1,15 +1,16 @@
 import requests
 
 def verify(mail):
-    response = requests.get("https://isitarealemail.com/api/email/validate",params = {'email': mail},headers = {'Authorization': "Bearer " + "21a3f468-1c3a-43d6-bdd6-d5437d6c0252" })
     try:
-        data = response.json()
-        status = data['status']
-        if status == "valid":
-            return "True"
-        elif status == "invalid":
-            return None
-        else:
+        response = requests.get("https://isitarealemail.com/api/email/validate",timeout=1.5,params = {'email': mail},headers = {'Authorization': "Bearer " + "21a3f468-1c3a-43d6-bdd6-d5437d6c0252" })
+        try:
+            data = response.json()
+            status = data['status']
+            if status == "valid":
+                return "True"
+            else:
+                return None
+        except:
             return None
     except:
         return None
